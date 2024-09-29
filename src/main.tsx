ba5829bed06b5afe2174d04350e168da3b63919a
compasstar <woohong24@naver.com>
2024-09-29 22:22:37 +0900

    [ADD] : 프로젝트 초기설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import Home from './pages/Home.tsx';
+import About from './pages/About.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      }
+    ]
+  }
+
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
